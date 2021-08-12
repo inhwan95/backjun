d3a0bcdee519cd80bb4f1c12ff9976e0be1363ed
inhwan95 <dldlsghks101@naver.com>
2021-08-12 20:52:40 +0900

    1일 1문제 자꾸 50점만 맞아서 답을 살짝

---
+++ backjun/src/backjun01/Hashing15829.java
@@ -0,0 +1,24 @@
+package backjun01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Hashing15829 {
+	static final int r=31;
+	static final int M=1234567891;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int L=Integer.parseInt(br.readLine());
+		String s=br.readLine();
+		long answer=0;
+		for(int i=0;i<L;i++){
+			answer+=(s.charAt(i)-'a'+1)*pow(r,i);
+		}
+		System.out.println(answer%M);
+	}
+	static long pow(int a,int b){
+		return b==0 ? 1 : a*pow(a,b-1)%M;
+			// true면 1이고 false면 pow ㄱ
+	}
+}
