0d35b6c28a3b808a5fc18b176127ed9df72ad8bb
inhwan95 <dldlsghks101@naver.com>
2021-08-15 21:18:51 +0900

    1백준 되라

--- backjun/src/backjun01/Main.java
+++ backjun/src/backjun01/Main.java
@@ -1,19 +1,40 @@
 package backjun01;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main{
-	public static void main(String[] args) throws IOException  {
+// 3키로와 5키로 봉지가 있다. 숫자N 킬로그램을 쟀을때 나오는 최소 봉지 수
+public class Main {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int num = Integer.parseInt(br.readLine());
-		String[] input = br.readLine().split("");
-	
-		int read = br.read();
-		System.out.println(read-96);
+
+		int box = Integer.parseInt(br.readLine());
+		int five = 0;
+		int three = 0;
+
+		if(box==3| box==6| box==9| box==12) {
+			while(box>=3) {
+				box -= 3;
+				three++;
+			}
+		} else if(box>=5) {	
+			while(box>=5) {
+				box -= 5;
+				five++;
+				if(box==3| box==6| box==9| box==12) {
+					while(box>=3) {
+						box -= 3;
+						three++;
+					}
+				} 
+			}
+		} else {
+			System.out.println(-1);
+			return;
+		}
+		if(box!=0) {
+			System.out.println(-1);
+			return;
+		}
+		System.out.println(five+three);
 	}
 }
